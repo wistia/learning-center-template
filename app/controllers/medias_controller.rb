9eef999ed9a4717e5c981dd45934bc92b133a33b
Alyce Currier <notalyce@gmail.com>
2012-12-17 14:42:56 -0500

    Turning things into a Rails project LOL

---
+++ app/controllers/medias_controller.rb
@@ -0,0 +1,4 @@
+class MediasController < ApplicationController
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningCenter::Application.config.secret_token = '9705dd42c4a365863c2e24dc0c9b6346442827a1f3f10233f9b8bc5558fc0909d64ae2b99a9863a7db4163b23d6bd0db36a0edc1cc9d617a93f94212c9ea3eab'
